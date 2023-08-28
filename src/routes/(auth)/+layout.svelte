1e248b682873a753e884278367783d2c6b9dd730
spences10 <spences10apps@gmail.com>
2023-08-28 22:47:46 +0100

    refactor: :recycle: messing with layouts

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,10 @@
+<div class="w-full h-full prose-xl">
+	<h2>Account Settings</h2>
+	<div class="divider" />
+</div>
+<div class="flex w-full h-full space-x-4">
+	<ul class="menu bg-base-100 w-60 px-2 rounded-box" />
+	<div class="w-full">
+		<slot />
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,17 @@
 	import '../app.postcss'
 </script>
 
-<slot />
+<form action="/logout" method="POST">
+	<li>
+		<button
+			type="submit"
+			class="btn-sm btn-warning text-warning-content"
+		>
+			Logout
+		</button>
+	</li>
+</form>
+
+<main class="container max-w-3xl mx-auto px-4 flex-grow">
+	<slot />
+</main>
