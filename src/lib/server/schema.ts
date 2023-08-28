32f28e4fb20de7eeb15fe781101e5c273138fcee
spences10 <spences10apps@gmail.com>
2023-08-28 22:47:22 +0100

    refactor: :recycle: update schema

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -50,3 +50,66 @@ export const userSession = sqliteTable('user_session', {
 })
 export const insertUserSessionSchema = createInsertSchema(userSession)
 export const selectUserSessionSchema = createSelectSchema(userSession)
+
+// Contacts Table Schema
+export const contacts = sqliteTable('contacts', {
+	contact_id: integer('contact_id').primaryKey(),
+	name: text('name').notNull(),
+	relationship: text('relationship'),
+	birthday: text('birthday'),
+	industry: text('industry'),
+	location: text('location'),
+	vip: integer('vip').default(0),
+	last_update: text('last_update'),
+	last_contacted: text('last_contacted'),
+	status: text('status').default('All Good'),
+})
+
+export const insertContactsSchema = createInsertSchema(contacts)
+export const selectContactsSchema = createSelectSchema(contacts)
+
+// Interactions Table Schema
+export const interactions = sqliteTable('interactions', {
+	interaction_id: integer('interaction_id').primaryKey(),
+	contact_id: integer('contact_id')
+		.notNull()
+		.references(() => contacts.contact_id),
+	type: text('type'),
+	date: text('date').notNull(),
+	notes: text('notes'),
+})
+
+export const insertInteractionsSchema =
+	createInsertSchema(interactions)
+export const selectInteractionsSchema =
+	createSelectSchema(interactions)
+
+// Background Table Schema
+export const background = sqliteTable('background', {
+	background_id: integer('background_id').primaryKey(),
+	contact_id: integer('contact_id')
+		.notNull()
+		.references(() => contacts.contact_id),
+	family: text('family'),
+	company: text('company'),
+	likes_dislikes: text('likes_dislikes'),
+	misc_notes: text('misc_notes'),
+})
+
+export const insertBackgroundSchema = createInsertSchema(background)
+export const selectBackgroundSchema = createSelectSchema(background)
+
+// ContactInfo Table Schema
+export const contactInfo = sqliteTable('contact_info', {
+	contact_info_id: integer('contact_info_id').primaryKey(),
+	contact_id: integer('contact_id')
+		.notNull()
+		.references(() => contacts.contact_id),
+	main_app: text('main_app'),
+	email: text('email'),
+	phone_number: text('phone_number'),
+	social_links: text('social_links'),
+})
+
+export const insertContactInfoSchema = createInsertSchema(contactInfo)
+export const selectContactInfoSchema = createSelectSchema(contactInfo)
