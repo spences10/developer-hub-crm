59ad5c4960df2eb76b5183cc1c484d2f2fa5e2f3
spences10 <spences10apps@gmail.com>
2023-08-30 21:21:24 +0100

    refactor: :recycle: layout shit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,17 +2,22 @@
 	import '../app.postcss'
 </script>
 
-<form action="/logout" method="POST">
-	<li>
-		<button
-			type="submit"
-			class="btn-sm btn-warning text-warning-content"
-		>
-			Logout
-		</button>
-	</li>
-</form>
-
 <main class="container max-w-3xl mx-auto px-4 flex-grow">
+	<form action="/logout" method="POST">
+		<li>
+			<button
+				type="submit"
+				class="btn-sm btn-warning text-warning-content"
+			>
+				Logout
+			</button>
+		</li>
+	</form>
+
+	<ul>
+		<li><a href="/contacts">Contacts</a></li>
+		<li><a href="/contacts/add">Add contact</a></li>
+	</ul>
+
 	<slot />
 </main>
