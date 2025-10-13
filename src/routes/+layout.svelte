e491644c756f83f4e412fe75e5ceeef3bdcf4547
spences10 <spences10apps@gmail.com>
2025-10-13 08:37:26 +0100

    feat: :sparkles: add fathom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
-	import { Head } from 'svead';
+	import { browser } from '$app/environment';
+	import { page } from '$app/state';
+	import {
+		PUBLIC_FATHOM_ID,
+		PUBLIC_FATHOM_URL,
+	} from '$env/static/public';
 	import favicon from '$lib/assets/favicon.svg';
 	import { seo_configs } from '$lib/seo';
+	import * as Fathom from 'fathom-client';
+	import { Head } from 'svead';
+	import { onMount } from 'svelte';
 	import '../app.css';
 
 	let { children } = $props();
+
+	onMount(() => {
+		Fathom.load(PUBLIC_FATHOM_ID, {
+			url: PUBLIC_FATHOM_URL,
+		});
+	});
+
+	// Track pageview on route change
+	$effect(() => {
+		(page.url.pathname, browser && Fathom.trackPageview());
+	});
 </script>
 
 <Head seo_config={seo_configs.home} />
