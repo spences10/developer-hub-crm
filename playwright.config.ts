2a0114bdd396dee973db25df39beae612220ba1f
spences10 <spences10apps@gmail.com>
2025-10-26 14:12:23 +0000

    feat: increase timeout in Playwright configuration to improve test reliability

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	timeout: 2000,
+	timeout: 5000,
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173,
